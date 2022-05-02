17016546a6b01327ef0c9464dec2feb8703a9cec
SantiagoEzequielMunioz <99147414+SantiagoEzequielMunioz@users.noreply.github.com>
2022-05-02 16:56:27 -0300

    Update settings.py

--- SitioWeb/SitioWeb/settings.py
+++ SitioWeb/SitioWeb/settings.py
@@ -147,5 +147,5 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST="smtp.gmail.com"
 EMAIL_USE_TLS=True
 EMAIL_PORT=587
-EMAIL_HOST_USER='san88.zzl.308@gmail.com'
-EMAIL_HOST_PASSWORD='sm180688'
+EMAIL_HOST_USER='xxxxx@gmail.com'
+EMAIL_HOST_PASSWORD='xxxxx'
